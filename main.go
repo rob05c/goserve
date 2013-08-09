4bed1a02601bf6659a6323d7bf557dd2a658c1a9
Robert Butts <robert.o.butts@gmail.com>
2013-08-09 10:53:09 -0500

    added second handler for testing

--- main.go
+++ main.go
@@ -9,8 +9,13 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to " + r.URL.Path[1:])
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the index! sub " + r.URL.Path[1:])
+}
+
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/index.html", indexHandler)
 	err := http.ListenAndServe(":80", nil)
 	if err != nil {
 		fmt.Println(err)

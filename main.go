6f7b54ebf2bc3c743d867f19734012538d2f363f
Robert Butts <robert.o.butts@gmail.com>
2013-08-17 22:53:48 -0500

    made it easier to add more files to be served, added the beginnings of a website

--- main.go
+++ main.go
@@ -19,19 +19,52 @@ func init() {
 	flag.Parse()
 }
 
+type ServePath struct {
+	Path string
+	Value []byte
+	ContentType string
+}
+
+func NewFileServePath(file string, contentType string) (*ServePath, error) {
+	value, err := ioutil.ReadFile(file)
+	return &ServePath {Path: file, ContentType: contentType, Value: value}, err
+}
+
 func main() {
-	index, err := ioutil.ReadFile("index.html")
-	if err != nil {
-		fmt.Println(err)
+	// only specific files are served. A user can't just request an arbitrary file.
+	// @todo put this in a config file
+	files := map[string] string {
+		"index.html": "text/html", // the first entry will be served at the root
+		"main.css": "text/css",
+		"reset.css": "text/css",
+	}
+	paths := make([]*ServePath, 0)
+	for file, contentType := range files {
+		path, err := NewFileServePath(file, contentType)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		paths = append(paths, path)
+	}
+
+	makeHandler := func(s []byte, contentType string) (func(w http.ResponseWriter, r *http.Request)) {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", contentType)
+			fmt.Fprintf(w, "%s", s)
+		}
+	}
+
+	if len(paths) == 0 {
+		fmt.Println("No paths to serve")
 		return
 	}
 
-	indexHandler := func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, string(index))
+	http.HandleFunc("/", makeHandler(paths[0].Value, paths[0].ContentType))
+	for _, path := range paths {
+		http.HandleFunc("/" + path.Path, makeHandler(path.Value, path.ContentType))
 	}
-
-	http.HandleFunc("/", indexHandler)
-	err = http.ListenAndServe(":" + strconv.Itoa(port), nil)
+	err := http.ListenAndServe(":" + strconv.Itoa(port), nil)
 	if err != nil {
 		fmt.Println(err)
 	}
